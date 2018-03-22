b01b27d059101854ee2fa44e6c03797849cdd213
s11rikuya <rikuya.suzuki@gaiax.com>
2018-03-22 16:06:40 +0900

    create editscreen

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import Appbar from './src/components/Appbar';
 import MemoListScreen from './src/screens/MemoListScreen'
 import MemoDetailScreen from './src/screens/MemoDetailScreen'
+import MemoEditScreen from './src/screens/MemoEditScreen'
 
 
 export default class App extends React.Component {
@@ -10,7 +11,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Appbar />
-        <MemoListScreen />
+        <MemoEditScreen />
       </View>
     );
   }
